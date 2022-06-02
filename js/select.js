e2be31b1d5b45880506c70c2e10c32710a0e6968
李佳 <523319121@qq.com>
2022-06-02 15:55:22 +0800

    '补全下拉框'

--- js/select.js
+++ js/select.js
@@ -12,5 +12,32 @@ quyuLi.onclick=function(){
 mBanner.onclick=function(){
     mBanner.style.display='none';
     quyu.style.display='none';
+    price.style.display='none';
+    hometype.style.display='none';
+    moreHome.style.display='none';
     
+}
+// 点击价格
+var priceLi=document.getElementsByTagName('li')[1];
+var price=document.getElementsByClassName('price')[0];
+priceLi.onclick=function(){
+    mBanner.style.display='block';
+    price.style.display='block';
+
+}
+// 点击房型
+var homeLi=document.getElementsByTagName('li')[2];
+var hometype=document.getElementsByClassName('hometype')[0];
+homeLi.onclick=function(){
+    mBanner.style.display='block';
+    hometype.style.display='block';
+
+}
+// 点击更多
+var moreLi=document.getElementsByTagName('li')[3];
+var moreHome=document.getElementsByClassName('moreHome')[0];
+moreLi.onclick=function(){
+    mBanner.style.display='block';
+    moreHome.style.display='block';
+
 }
